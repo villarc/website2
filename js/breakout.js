c8394bdf94c0fb6b2feaae65550d4458fcbf7e01
villarc <142903187+villarc@users.noreply.github.com>
2024-04-04 03:35:26 +0000

    Wed, Apr 3, 2024, 8:35 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,7 +1,65 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
 
+score = 0
+
+brickRowCount = 9
+brickColumnCount = 5
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+//Create paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginpath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#009599'
+    ctx.fill()
+    ctx.closePath()
+}
+
+// Draw paddle on canvas
+function drawPaddle() {
+    ctx.beginpath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#009599'
+    ctx.fill()
+    ctx.closePath()
+}
+
+
+//Draw score on canvas
+function drawScore() {
+    ctx.font ='20px Arial'
+    ctx.fillText(`Score:${score}`, canvas.width-100, 30)
+}
+
+//Draw everything
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+}
 
 //Rules open and closeevent handlers
 rulesBtn.addEventListener('click', () => {
